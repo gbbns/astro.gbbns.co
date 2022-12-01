a472ef6139cdd85c78f0b19fcb858147a4542dc0
Chris Gibbons <chris@gbbns.co>
2022-12-01 15:54:48 +0000

    feat: adds in global meta content config

---
+++ src/data/siteMetaData.mjs
@@ -0,0 +1,28 @@
+export const siteMetaData = {
+  siteTitle: `UX Technologist`,
+  siteShortTitle: `Front-end & Accessibility, with a side of coffee & cake`,
+  siteDescription: `Principal UI/front end developer & UX technologist based in Blackpool // Manchester`,
+  siteIntroTitle: `Hello, I'm Chris Gibbons.`,
+  siteHomePageIntroTitle: `Principal Front-end developer, Engineering Practice Lead, Amateur cyclist, Photography enthusiast, Wanna be writer and Occasional baker.`,
+  siteTagline: `I’m a UX Technologist and principal UI/front end developer from the UK.`,
+  siteUrl: `https://www.gbbns.co`,
+  siteUrlSlug: `gbbns.co`,
+  siteRepo: `https://github.com/gbbns/`,
+  siteLogo: `/static/images/logo.png`,
+  siteLanguage: `en-gb`,
+  siteLocale: `en-GB`,
+
+  author: {
+    name: `Chris Gibbons`,
+    email: `chris@gbbns.co`,
+    image: `/static/images/profile/chris-gibbons.jpeg`,
+  },
+
+  social: {
+    banner: `/static/images/twitter-card.png`,
+    github: `https://github.com/_gbbns`,
+    twitter: `https://twitter.com/_gbbns`,
+    instagram: `https://instagram.com/_gbbns`,
+    linkedin: `https://www.linkedin.com/in/gbbns/`,
+  },
+}
