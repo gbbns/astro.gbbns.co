680c5eb93f45e480eb70ef9845ec4a362506ca81
Chris Gibbons <chris@gbbns.co>
2023-01-05 13:53:24 +0000

    feat: adds in sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx'
+
+// https://astro.build/config
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
-});
+  site: 'https://gbbns.co',
+  integrations: [mdx(), sitemap()],
+})
