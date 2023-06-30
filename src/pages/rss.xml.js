9c2b9db680ad900c51677ada86712b77628e6187
Chris Gibbons <gbbns@users.noreply.github.com>
2023-06-30 11:12:24 +0100

    feat: update rss.xml generator

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,21 @@
 import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
 
-export const get = () =>
-  rss({
+export async function get(context) {
+  const blog = await getCollection('blog')
+  return rss({
     title: 'Chris Gibbons | gbbns.co',
-    description: 'Web stuff',
-    site: 'https://www.gbbns.co',
-    items: import.meta.glob('./**/*.md'),
-    customData: `<language>en-GB</language>`,
+    description:
+      'Web stuff, old school front-end, accessibility and lots of waffling',
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      customData: `<language>en-GB</language>`,
+      // Compute RSS link from post `slug`
+      // This example assumes all posts are rendered as `/blog/[slug]` routes
+      link: `/blog/${post.slug}/`,
+    })),
   })
+}
